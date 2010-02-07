6dd47daec7892c3b087425d99df0b0b060df06c3
Adam Russell <a6russell@gmail.com>
2010-02-07 02:44:52 -0500

    Added FAQs page.

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,23 @@
 from django.conf.urls.defaults import *
 
+whats_new_info = {}
+
 urlpatterns = patterns(
     'madscientist.home.views',
 
-    url(r'^$', 'home'),
+    url(r'^$', 'home', name='home'),
+)
+
+urlpatterns += patterns(
+    'django.views.generic.simple',
+
+    url(r'^faq/$', 'direct_to_template',
+        {'template': 'home/faq.html',}, 'faq'),
+)
+
+urlpatterns += patterns(
+    'django.views.generic.list_detail',
+
+    url(r'^whats_new/$', 'object_list', whats_new_info, 'whats_new'),
 )
 
